2efb4f5b2e6d1c029aed462922b83c1af44ad8f1
Milan Pandurov <milanpandurov@pm.me>
2018-12-05 10:35:47 +0100

    Temorary removes closing of the chanel

--- go/sibrad/resvmgr/resolver.go
+++ go/sibrad/resvmgr/resolver.go
@@ -452,6 +452,7 @@ func (r *resolver) refire(wait time.Duration) {
 func (r *resolver) quit(err error) {
 	r.mu.Lock()
 	defer r.mu.Unlock()
+	log.Debug("Sending quit message to events chanel")
 	if !r.closed {
 		r.quitChan <- quitSignal{
 			key: r.key,
@@ -461,7 +462,7 @@ func (r *resolver) quit(err error) {
 			Code:  Quit,
 			Error: err,
 		}
-		close(r.events)
+		//close(r.events)
 		r.closed = true
 	}
 }
