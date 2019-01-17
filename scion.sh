b1711994926b8b5ceab8d4df9f9b4be5084d27e5
Matthias Frei <matthias.frei@inf.ethz.ch>
2019-01-17 16:44:54 +0100

    Add non-docker option for zookeeper back (#19)

--- scion.sh
+++ scion.sh
@@ -93,7 +93,12 @@ run_zk() {
         return
     fi
     echo "Running zookeeper..."
-    ./tools/quiet ./tools/dc zk up -d
+    if is_docker_zk; then
+        host_zk_stop
+        ./tools/quiet ./tools/dc zk up -d
+    else
+        host_zk_start
+    fi
     if [ -n "$1" ]; then
         echo "Deleting all Zookeeper state"
         # Wait some time, such that zookeeper accepts connections again after startup
@@ -101,7 +106,7 @@ run_zk() {
         local addr="127.0.0.1:2181"
         if is_running_in_docker; then
             addr="${DOCKER0:-172.17.0.1}:2182"
-        elif is_docker_be; then
+        elif is_docker_zk; then
             addr="$(./tools/docker-ip):2181"
         fi
         tools/zkcleanslate --zk "$addr"
@@ -122,6 +127,14 @@ stop_jaeger() {
     fi
     echo "Stopping jaeger..."
     ./tools/quiet ./tools/dc jaeger down
+}
+
+host_zk_start() {
+    systemctl is-active --quiet zookeeper || sudo -p "Starting local zk - [sudo] password for %p: " systemctl start zookeeper
+}
+
+host_zk_stop() {
+    systemctl is-active --quiet zookeeper && sudo -p "Stopping local zk - [sudo] password for %p: " systemctl stop zookeeper
 }
 
 cmd_mstart() {
@@ -241,6 +254,10 @@ glob_match() {
 
 is_docker_be() {
     [ -f gen/scion-dc.yml ]
+}
+
+is_docker_zk() {
+    is_docker_be || [ -f gen/zk-dc.yml ]
 }
 
 is_supervisor() {
